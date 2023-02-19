727175636350c8efc04b062976acb79694f469e5
gogamid <gamidliimron@gmail.com>
2023-02-19 14:19:03 +0100

    migrate from previous project blatt3 till blatt9 half

---
+++ blatt4-mnist_code_skeleton/mnist_code_skeleton.py
@@ -0,0 +1,26 @@
+import sys
+
+import numpy as np
+import requests
+import os
+
+if __name__ == "__main__":
+
+    ## download MNIST if not present in current dir!
+    if os.path.exists("./mnist.npz") == False:
+        print("Downloading MNIST...");
+        fname = 'mnist.npz'
+        url = 'http://www.gepperth.net/alexander/downloads/'
+        r = requests.get(url + fname)
+        open(fname, 'wb').write(r.content)
+
+    ## read it into 'traind' and 'trainl'
+    data = np.load("mnist.npz")
+    traind = data["arr_0"];
+    trainl = data["arr_2"];
+
+    if sys.argv[1] == "1":
+        pass;
+
+    if sys.argv[1] == "2":
+        pass;
