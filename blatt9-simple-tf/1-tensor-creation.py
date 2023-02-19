5ed31b98c7dceb8b399b3c6c6a131ca3550c459f
gogamid <gamidliimron@gmail.com>
2023-02-19 17:07:45 +0100

    blatt 9 reshaping

--- blatt9-simple-tf/1-tensor-creation.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-print(tf.__version__)
-print("########## a #################")
-# # Create a 1D tensor with entries from 0 to 100 in steps of 2
-# a = tf.range(0, 101, 2)
-# print(a)
